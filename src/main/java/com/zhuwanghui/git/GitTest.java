253fc1ee4fd2251e13f92750ab411bb968e8c5cb
Zhu <2567295875@qq.com>
2022-09-14 21:26:06 +0800

    third commit

--- src/main/java/com/zhuwanghui/git/GitTest.java
+++ src/main/java/com/zhuwanghui/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello ZhuWanghui");
+        System.out.println("Hello ZhangZitong");
     }
 }
